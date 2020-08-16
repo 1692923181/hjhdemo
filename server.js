7f4b871420a0ad2d4b46ffbb4985cdc334a73d9e
Hou1002625505 <1002625505@qq.com>
2020-08-16 12:46:56 +0800

    注册页面准备

---
+++ server.js
@@ -0,0 +1,24 @@
+const express =require('express')
+const cookieParser = require('cookie-parser')
+const app = express()
+const userRouter = require('./routers/user')
+
+//模板引擎
+app.set('views','views')
+app.set('view engine','ejs')
+
+//静态资源托管
+app.use(express.static('public'))
+
+//req.body
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
+//req,cookies
+app.use(cookieParser())
+
+//路由
+app.use('/users',userRouter)
+
+app.listen(3000)
